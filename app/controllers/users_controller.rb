8995c68e30c73c974a5c198fc5fa3d8904dfef28
tcrurup <tcrurup@gmail.com>
2019-09-13 11:08:11 -0700

    update sign in view with fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
 
+    def new
+        @user = User.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   
   get '/login' => 'sessions#new'
 
-  resources :users, only: [:new]
+  resources :users, only: [:create, :new]
 end
