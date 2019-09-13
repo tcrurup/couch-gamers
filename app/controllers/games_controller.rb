a56f51e8389f08fb665ab7f8f57bbd669ecd9476
tcrurup <tcrurup@gmail.com>
2019-09-13 15:22:22 -0700

    add strong params to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,16 @@
 class GamesController < ApplicationController
 
+    def create
+        @game = Game.create(game_params)
+    end
+    
     def new
         @game = Game.new
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :description, :release_year)
     end
 end
