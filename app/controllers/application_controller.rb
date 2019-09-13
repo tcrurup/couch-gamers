cce260204f703767f96161e26a3bc4b860bad819
tcrurup <tcrurup@gmail.com>
2019-09-13 13:26:10 -0700

    redirect user on login if already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
     def current_user
         User.find(session[:user_id])
     end
+
+    private
+
+    def logged_in?
+        !!current_user
+    end
 end
