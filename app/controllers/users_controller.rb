559c5ffe1266d779351be7bfb19f69d869bcabef
tcrurup <tcrurup@gmail.com>
2019-09-13 11:14:43 -0700

    can create users from sign up page and go to users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
+    def create
+        @user = User.create(user_params)
+        puts params
+        puts user_params
+        redirect_to user_path(@user)
+    end
+    
     def new
         @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :first_name, :last_name)
     end
 end
