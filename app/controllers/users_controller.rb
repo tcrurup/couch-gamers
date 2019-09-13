5f0b635d34e5814bd02347693be09b430892371a
tcrurup <tcrurup@gmail.com>
2019-09-13 14:01:36 -0700

    when signing up the form wil re render if not valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
     def create
-        @user = User.create(user_params)
-        redirect_to user_path(@user)
+        @user = User.new(user_params)
+
+        if @user.valid?
+            @user.save
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
     
     def new
