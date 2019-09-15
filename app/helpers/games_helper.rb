63b1334196b8719805ea9175fabaf5edf51efbff
tcrurup <tcrurup@gmail.com>
2019-09-15 10:42:54 -0700

    refactor games_card into helper functions for clarity;

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,4 +1,15 @@
 module GamesHelper
 
+    def show_member_count_as_link_to_developer_game_user_path_for (game)
+        link_to "#{game.user_count} members own this game", developer_game_users_path(game.developer_id, game)
+    end
+
+    def show_add_game_button_for(game)
+        if current_user.has_game?(game)
+            button_tag "You Own This Game", disabled: true 
+        else
+            button_to "Add To My Games", "/add_to_current_users_games/#{game.id}" 
+        end
+    end
 
 end
