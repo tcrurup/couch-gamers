d3ed45e72a8073d1d652705ff77e3d814f8d0c1d
tcrurup <tcrurup@gmail.com>
2019-09-14 22:28:29 -0700

    creating an account now logs user in immediately

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,9 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-
         if @user.valid?
             @user.save
-            redirect_to user_path(@user)
+            login(@user)
         else
             render :new
         end
@@ -24,6 +23,11 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def login(user)
+        session[:user_id] = user.id
+        redirect_to user_path(@user)
+    end
 
     def user_params
         params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
