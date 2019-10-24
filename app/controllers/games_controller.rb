d2b09372c1f35a0b39972117590fcbac6068ff85
tcrurup <tcrurup@gmail.com>
2019-10-24 00:26:55 -0700

    rafactor code in game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,36 +2,27 @@ class GamesController < ApplicationController
     
     #User must be logged in before an actions in the game contoller are allowed to take place
     before_action :require_login
-    before_action :set_developer_by_id, :except => [:edit]
-    before_action :set_game_by_id, :only => [:destroy, :edit, :update]
+    before_action :set_instance_variables
+
 
 
 
     def create
         @game = @developer.new_game(game_params)
-        
-        if current_user_can_CRUD? && @game.valid?
-            save_game_and_redirect
-        else
-            render :new
-        end
+        current_user_can_CRUD? && @game.valid? ? (save_game_and_redirect) : (render :new)
     end
 
     def destroy  
-        if current_user_can_CRUD? 
-            delete_game 
-        else 
-            redirect_to user_path(current_user)
-        end       
+        current_user_can_CRUD? ? (delete_game_and_redirect) : (redirect_to user_path(current_user))    
     end
 
     def edit
-        render :show unless current_user_can_CRUD?
+        redirect_to developer_game_path(@developer, @game) unless current_user_can_CRUD?
     end
     
     def new
         @game = Game.new(developer_id: @developer.id)
-        render "developers/show" unless current_user_can_CRUD?  
+        redirect_to developer_path(@developer) unless current_user_can_CRUD?  
     end
 
     def index
@@ -39,29 +30,16 @@ class GamesController < ApplicationController
     end
 
     def show
-        @game = @developer.games.find_by(id: params[:id])
-        if @game.nil? 
-            message = "#{@developer.name} does not have a game with that id"
-            flash_and_redirect_to_show_page(@developer, message)
-        end                
+        flash_and_redirect_to_show_page(@developer,"#{@developer.name} does not have a game with that id") if @game.nil?         
     end
 
     def update
-        @game.update(game_params)
-
-        if @game.valid?
-            redirect_to developer_game_path(@game.developer, @game)
-        else
-            render :edit
-        end
+        @game.assign_attributes(game_params)
+        @game.valid? ? (save_game_and_redirect) : (render :edit)
     end
 
     private
-    def current_user_employed_at_developer
-        @developer.has_employee?(current_user)
-    end 
-
-    def delete_game_and_redirect
+    def delete_game_and_redirect_to_developer
         @game.destroy       
         flash_and_redirect_to_show_page(@game.developer,"#{@game.title} has been removed")
     end
@@ -83,24 +61,15 @@ class GamesController < ApplicationController
         redirect_to developer_game_path(@game.developer, @game)
     end
 
-    def set_developer_by_id
+    def set_instance_variables
         @developer = Developer.find_by(id: params[:developer_id])
-    end
-
-    def set_game_by_id
-        @game = Game.find_by(id: params[:id])
-    end    
-
-    def set_instance_variable
-        @developer = Developer.find_by(id: params[:developer_id])
-        if @developer
+        if params[:id]
             @game = @developer.games.find_by(id: params[:id])
-        else
-            @game = Game.find_by(id: params[:id])
         end
     end
 
     def current_user_can_CRUD?
+        #User can CRUD a game if the are an employee for the developer
         @game.user_has_permission_to_CRUD?(current_user)
     end
 
